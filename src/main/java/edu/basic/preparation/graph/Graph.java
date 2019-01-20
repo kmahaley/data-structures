0b6790b187f5c69291e666439e1f5f6c2b932afc
Mahaley, Kartik <Kartik.Mahaley@ncr.com>
2019-01-20 10:46:57 -0500

    Move dfs solution to graph class

--- src/main/java/edu/basic/preparation/graph/Graph.java
+++ src/main/java/edu/basic/preparation/graph/Graph.java
@@ -357,4 +357,134 @@ public class Graph {
 //        }
 //    }
 
+    /**
+     *
+     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+     * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+     * You may assume all four edges of the grid are all surrounded by water.
+     * Input:
+     * 11000
+     * 11000
+     * 00100
+     * 00011
+     *
+     * Output: 3
+     */
+    public static int numIslands(char[][] grid) {
+
+        int count = 0;
+        if(grid == null || grid.length == 0) {
+            return count;
+        }
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    dfsNumIslands(i, j, grid, visited);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     *
+     * @param i row
+     * @param j col
+     * @param grid matrix
+     * @param visited visited matrix
+     */
+    private static void dfsNumIslands(int i, int j, char[][] grid, boolean[][] visited) {
+
+        if (isValidInMatrix(i, j, grid) && !visited[i][j] && grid[i][j] == '1') {
+            visited[i][j] = true;
+            dfsNumIslands(i + 1, j, grid, visited); //below
+            dfsNumIslands(i - 1, j, grid, visited); //above
+            dfsNumIslands(i, j + 1, grid, visited); //right
+            dfsNumIslands(i, j - 1, grid, visited); //left
+        } else {
+            return;
+        }
+
+    }
+
+    /**
+     * Check if item belows to matrix
+     */
+    private static boolean isValidInMatrix(int i, int j, char[][] matrix) {
+        return i >= 0 && i < matrix.length &&
+                j >= 0 && j < matrix[0].length;
+    }
+
+    /**
+     * Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+     *
+     * A region is captured by flipping all 'O's into 'X's in that surrounded region.
+     * Example:
+     *
+     * X X X X
+     * X O O X
+     * X X O X
+     * X O X X
+     * After running your function, the board should be:
+     *
+     * X X X X
+     * X X X X
+     * X X X X
+     * X O X X
+     */
+    public static void solve(char[][] board) {
+
+        if( board == null || board.length == 0){
+            return;
+        }
+        int length = board.length, width = board[0].length;
+
+//        find border 'O', first and last column
+        for (int i = 0; i < length ; i++) {
+            if(board[i][0] == 'O') {
+                dfsSolveBoard(i,0, board);//first column
+            }
+            if(board[i][width-1] == 'O') {
+                dfsSolveBoard(i, width-1, board);// last column
+            }
+        }
+//      find first and last row 'O'
+        for (int j = 0; j < width; j++) {
+            if(board[0][j] == 'O') {
+                dfsSolveBoard(0, j, board);// first row
+            }
+            if(board[length-1][j] == 'O') {
+                dfsSolveBoard(length-1, j, board);// last row
+            }
+        }
+//      set matrix values to X if not #, and convert # to O
+        for (int i = 0; i < length ; i++) {
+            for (int j = 0; j < width; j++) {
+                if(board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if(board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    /**
+     * Set border O to # using dfs search
+     */
+    private static void dfsSolveBoard(int row, int col, char[][] board) {
+
+        if(isValidInMatrix(row, col, board) && board[row][col] == 'O') {
+            board[row][col] = '#';
+            dfsSolveBoard(row-1, col, board);//above
+            dfsSolveBoard(row+1, col, board);//below
+            dfsSolveBoard(row, col-1, board);//left
+            dfsSolveBoard(row, col+1, board);//right
+        }
+    }
+
 }
